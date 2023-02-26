4a98f784170b4823ebcb447509b7b62424018e9b
Alek <KarpuninAlek@users.noreply.github.com>
2023-02-26 14:07:30 +0000

    Initial Commit

---
+++ Ingridients/IngridientsApp.swift
@@ -0,0 +1,17 @@
+//
+//  IngridientsApp.swift
+//  Ingridients
+//
+//  Created by M on 26/02/2023.
+//
+
+import SwiftUI
+
+@main
+struct IngridientsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
